b2091bd3c5c6133c58059bcbf33215c2db38f46d
Microseversk <alexander.osipov.work@gmail.com>
2024-07-02 23:22:10 +0700

    Добавил Input

---
+++ src/shared/Input/Input.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import styles from './styles.module.scss';
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+  isError?: boolean;
+  lines?: number;
+}
+const Input = ({ isError, lines = 1, ...props }: InputProps) => {
+  const inputClasses = classNames(styles['input'], { [styles['input_error']]: isError }, props.className);
+
+  if (lines > 1) {
+    return (
+      <textarea className={inputClasses} rows={lines} {...(props as React.ComponentPropsWithoutRef<'textarea'>)} />
+    );
+  }
+  return <input className={inputClasses} {...props} />;
+};
+
+export default Input;
