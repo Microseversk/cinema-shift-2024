9ac35c7235bc6ff1d671b51fa04a00eaeaa0cdf1
Microseversk <alexander.osipov.work@gmail.com>
2024-07-04 01:06:03 +0700

    Добавил скелетон для карточек фильма

--- src/Components/Pages/RootPage/RootPage.tsx
+++ src/Components/Pages/RootPage/RootPage.tsx
@@ -1,14 +1,25 @@
 import { useGetTodayQuery } from '../../../hooks/useGetTodayQuery';
 import { Typography } from '../../../shared';
 import { FilmCard } from './Components';
+import { FilmCardSkeleton } from './Components/FilmCard/FilmCardSkeleton';
 import styles from './styles.module.scss';
 export const RootPage = () => {
   const { data, isLoading } = useGetTodayQuery();
-  if (isLoading) return <div>Загрузка...</div>;
   return (
     <>
       <Typography variant="h2">Афиша</Typography>
-      <div className={styles.cards_wrapper}>{data?.films.map((film) => <FilmCard key={film.id} film={film} />)}</div>
+
+      <div className={styles.cards_wrapper}>
+        {isLoading ? (
+          <>
+            {[...Array(6)].map((_, index) => (
+              <FilmCardSkeleton key={index} />
+            ))}
+          </>
+        ) : (
+          <>{data?.films.map((film) => <FilmCard key={film.id} film={film} />)}</>
+        )}
+      </div>
     </>
   );
 };
