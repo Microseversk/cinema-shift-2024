6dd4a59ffb64c756d7efb8dd2835d642d7fc959b
Microseversk <alexander.osipov.work@gmail.com>
2024-07-04 02:16:40 +0700

    Убрал логи

--- src/shared/FilmRating/FilmRating.tsx
+++ src/shared/FilmRating/FilmRating.tsx
@@ -5,13 +5,11 @@ interface FilmRatingProps {
 }
 
 export const FilmRating = ({ rating }: FilmRatingProps) => {
-  console.log('----');
   return (
     <div className={styles.rating_wrapper}>
       {[0, 1, 2, 3, 4].map((num) => {
         const rate = rating - 2 * num;
         const width = rate > 2 ? '100%' : `${rate * 50}%`;
-        console.log(rating, num, rate, width);
         return (
           <div className={styles.star} key={num}>
             <div className={styles.star_filled} style={{ width: width }}></div>
