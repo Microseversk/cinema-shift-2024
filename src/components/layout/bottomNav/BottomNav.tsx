f739fd7d9acfa6fde45e0998002d4846d22e2974
Microseversk <alexander.osipov.work@gmail.com>
2024-07-11 17:41:17 +0700

    Адаптив для страницы фильма

--- src/components/layout/bottomNav/BottomNav.tsx
+++ src/components/layout/bottomNav/BottomNav.tsx
@@ -1,4 +1,5 @@
 import { MovieIcon, Ticket, Typography, User } from '@src/shared';
+import { ROUTES } from '@src/utils';
 import { NAVIGATE_ROUTES } from '@src/utils/constants/navigateRoutes';
 import { useLocation } from 'react-router-dom';
 import { NavLinkItem } from '../header/navLinkItem/NavLinkItem';
@@ -6,6 +7,9 @@ import styles from './styles.module.scss';
 
 export const BottomNav = () => {
   const { pathname } = useLocation();
+
+  if (![ROUTES.MAIN, ROUTES.PROFILE, ROUTES.ORDERS].includes(pathname)) return null;
+
   return (
     <footer className={styles.wrapper}>
       <ul className={styles.list}>
